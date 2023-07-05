7bfca47450c40e86725613b1765a682e4ade1982
railgun-0402 <april-life.f-441-2011@docomo.ne.jp>
2023-07-05 23:25:24 +0900

    top screen create

---
+++ src/components/BoardClass.vue
@@ -0,0 +1,18 @@
+<template>
+    <!-- ここにHTMLを書きます -->
+    <div>
+        <header>
+            my Trello
+        </header>
+        <main>
+            <p class="info-line">All: 0 tasks</p>
+        </main>
+    </div>
+</template>
+    
+<script>
+// ここにJSを書きます
+export default {
+    
+}
+</script>
